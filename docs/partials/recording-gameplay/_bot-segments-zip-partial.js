65d48fda8df011bedea8175e707113d1e9bac277
Abby Beizer <abby@regression.gg>
2024-09-26 15:43:38 -0400

    better wording for bot segments zip

--- docs/partials/recording-gameplay/_bot-segments-zip-partial.js
+++ docs/partials/recording-gameplay/_bot-segments-zip-partial.js
@@ -1,11 +1,12 @@
 const text = `
 
-This .zip contains numbered JSON files, each representing a delta of the game state at a specific tick.
+This .zip contains numbered JSON files, each representing actions that were taken since the previous tick
+(such as sending mouse and keyboard inputs to control character behaviours), 
+and a set of game state values that may have been affected by those actions.
 These files are used to [replay the recording](#replaying-recordings).
 
 [Bot Segments](bot-sequences/getting-started-with-bot-sequences-and-segments) are explained in more detail in their own section,
-but they essentially represent actions for the replay tool to produce (such as sending mouse and keyboard inputs),
-and outline conditions that should result from those actions.
+but they essentially represent actions for the replay tool to reproduce and outline conditions that should result from those actions.
 Bot Segments and Sequences are powerful tools for automating testing, and
 the contents of the Segments produced during recording can help you form the basis for your own custom Segments later on.
 
