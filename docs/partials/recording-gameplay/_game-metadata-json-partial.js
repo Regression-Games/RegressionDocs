e47aad090142e9e861ff59eee08f2ceff1fc89e1
Abby Beizer <abbybeizer@gmail.com>
2024-09-30 14:36:58 -0400

    add game_metadata.json description

---
+++ docs/partials/recording-gameplay/_game-metadata-json-partial.js
@@ -0,0 +1,10 @@
+const text = `
+
+This .json file contains metadata about your game such as its title, studio/publisher, editor version,
+and which input systems and rendering pipelines it utilizes.
+This information can be useful to Regression Games for debugging compatibility issues 
+between our SDK and your game.
+
+`;
+
+export default text;
